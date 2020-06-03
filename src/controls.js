07d6590044979611501545b6bf723da9d80c548f
Vivek Jagannath <vivekj1203@gmail.com>
2020-06-03 22:24:45 +0530

    touch game start fixed

--- src/controls.js
+++ src/controls.js
@@ -12,6 +12,12 @@ var Controls = function (game, canvas, con) {
 
     canvas.ontouchmove = function (e) {
         if (e.touches) {
+            if (game.gameScreen == GameScreen.gameStart) {
+                if (e.touches[0].pageX - canvas.offsetLeft >= game.object.x && e.touches[0].pageX - canvas.offsetLeft <= game.object.x + game.objectWidth && e.touches[0].pageY - canvas.offsetTop >= game.object.y && e.touches[0].pageY - canvas.offsetTop <= game.object.y + game.objectHeight) {
+                    game.gameScreen = GameScreen.level1;
+                    game.draw(con);
+                }
+            }
             game.object.update(e.touches[0].pageX, e.touches[0].pageY);
             e.preventDefault();
         }
